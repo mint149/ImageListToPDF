356809f3aa5ae82e2aea7c5aed6aea67a5eb6944
Mint <skylove.exe@gmail.com>
2021-03-21 00:00:38 +0900

    AppDelegateでの共有メニュー処理仮実装(動作せず)

--- ImageListToPDF/ImageListToPDFApp.swift
+++ ImageListToPDF/ImageListToPDFApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct ImageListToPDFApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("in!")
+        print("url: ", url)
+        
+        // ファイルを扱う処理
+        // ...
+        
+        return true
     }
 }
