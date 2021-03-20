a1701fba6b7b79092d31e05804fb160c7c14c227
Mint <skylove.exe@gmail.com>
2021-03-20 10:07:04 +0900

    Initial Commit

---
+++ ImageListToPDF/ImageListToPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListToPDFApp.swift
+//  ImageListToPDF
+//
+//  Created by hato on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ImageListToPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
